8337a6614cad30b3dab237c022975ce1f66b800b
Guilherme Padovani Silveira <64567292+GuilhermeSilveira93@users.noreply.github.com>
2023-12-05 18:53:04 -0300

    1

---
+++ src/app/_layout.tsx
@@ -0,0 +1,40 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+export const unstable_settings = {
+  initialRouteName: "/(tabs)/index",
+};
+SplashScreen.preventAutoHideAsync();
+export default function RootLayout() {
+  const [loaded, error] = useFonts({ ...FontAwesome.font });
+
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  return (
+    <>
+      <SafeAreaProvider>
+        <Stack>
+          <Stack.Screen name="index" />
+        </Stack>
+        <StatusBar style="auto" animated />
+      </SafeAreaProvider>
+    </>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native"
+
+export const Home = () => {
+  return (
+    <Text>Olá Mundo</Text>
+  )
+}
+export default Home
